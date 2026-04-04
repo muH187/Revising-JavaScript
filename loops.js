fe29dc99e6e7d00b15f580b80ec4d7776acab1fc
Muhammad Ali <proqualitythings100@gmail.com>
2026-04-04 12:28:13 +0500

    loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+let a = 1;
+
+for (let i = 0; i < 10; i++) {
+    console.log(a + i);
+}
+
+let myName = "Muhammad Ali"
+for (const element of myName) {
+    console.log(element)
+}
+
+
+let myNumber = 0;
+while (myNumber < 10) {
+    console.log(myNumber)
+    myNumber++
+}
