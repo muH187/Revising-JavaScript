8ffcd044c9ed024c6fa71e4c084d7d62ba0f9ed2
Muhammad Ali <proqualitythings100@gmail.com>
2026-05-08 13:27:30 +0500

    string split method

--- stringMethodPract.js
+++ stringMethodPract.js
@@ -6,3 +6,10 @@ const reverseString = reverse("Muhammad");
 
 console.log(reverseString)
 
+let myName = "Ali"
+let fullName = "Muhammad Ali Khan"
+
+console.log(myName.split());
+console.log(fullName.split(" ")) // every word will be an array
+console.log(fullName.split()) // will be a single array
+console.log(fullName.split("")) // every letter will be an array
