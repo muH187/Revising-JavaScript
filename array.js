519a2ebcaa240dde247d353aff4a2df66471ccdd
Muhammad Ali <proqualitythings100@gmail.com>
2026-05-08 16:00:22 +0500

    array

---
+++ array.js
@@ -0,0 +1,25 @@
+let array = [1, 2, 3, 4, 5]
+
+console.log(array)
+console.log(array.toString())
+console.log(array.join(" and "))
+
+let a = [100, 219, 53, 123, 320, 88, 80]
+
+console.log(a.pop()) 
+console.log(a.push(300)) 
+console.log(a.push("Ali"))
+
+
+console.log(a.shift())
+console.log(a.unshift("Ali"))
+
+console.log(a)
+
+let a1 = [2, 3, 5, 6]
+let a2 = ["Ali", "Mirza", "Wali", "Umer"]
+let a3 = [100, 80, 70, 60]
+
+console.log(a1.concat(a2, a3))
+
+console.log(a3.splice(1, 2))
